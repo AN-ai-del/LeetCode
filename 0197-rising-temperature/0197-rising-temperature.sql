561e6f291f8e49b2f34cbfc7eee70ab1dca3188c
Anushka Das <77006271+AN-ai-del@users.noreply.github.com>
2025-08-08 14:29:32 +0530

    Time: 485 ms (53.94%), Space: 0B (100%) - LeetHub

---
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+SELECT a.id as Id
+FROM Weather a
+INNER JOIN Weather b
+ON DATEDIFF(a.recordDate, b.recordDate)=1
+WHERE a.temperature > b.temperature
+ORDER BY id;
