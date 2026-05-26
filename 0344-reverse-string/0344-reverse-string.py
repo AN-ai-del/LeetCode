2df305a69719c812f52bb049ea164717ae59b112
Anushka Das <77006271+AN-ai-del@users.noreply.github.com>
2026-05-26 20:43:25 +0530

    Time: 0 ms (100%), Space: 23.3 MB (80.83%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.py
@@ -0,0 +1,12 @@
+class Solution:
+    def reverseString(self, s):
+        left = 0
+        right = len(s) - 1
+
+        while left < right:
+            # Swap characters
+            s[left], s[right] = s[right], s[left]
+
+            # Move pointers
+            left += 1
+            right -= 1
